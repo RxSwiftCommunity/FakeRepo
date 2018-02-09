254c093837e2f76c12ded34c7a285a46ac9b9e7a
Shai Mishali <freak4pc@gmail.com>
2018-02-09 22:25:42 +0200

    Add FakeExample

---
+++ FakeExample/FakeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FakeExample
+//
+//  Created by Shai Mishali on 2/9/18.
+//  Copyright © 2018 Shai Mishali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
